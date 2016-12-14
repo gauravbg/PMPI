a3556f6afe306143d7170d2b28cd211ef55f7f37
Amogh Avadhani <avadhaniamogh@bitbucket.org>
2016-12-14 15:26:05 -0500

    Getting all matches for a particular date

--- PMPI/src/com/project/ai/dataclasses/MatchInfo.java
+++ PMPI/src/com/project/ai/dataclasses/MatchInfo.java
@@ -2,14 +2,57 @@ package com.project.ai.dataclasses;
 
 public class MatchInfo {
 
-	String homeTeamName;
-	String awayTeamName;
+	String homeTeamLongName;
+	String awayTeamLongName;
+	String homeTeamShortName;
+	String awayTeamShortName;
 	String homeTeamId;
 	String awayTeamId;
 	String matchId;
-	int homeTeamPoints;
-	int awayTeamPoints;
-	int noOfMatchesPlayedHometeam;
-	int noOfMatchesPlayedAwayteam;
-	
+
+	public String getHomeTeamId() {
+		return homeTeamId;
+	}
+	public void setHomeTeamId(String homeTeamId) {
+		this.homeTeamId = homeTeamId;
+	}
+	public String getAwayTeamId() {
+		return awayTeamId;
+	}
+	public void setAwayTeamId(String awayTeamId) {
+		this.awayTeamId = awayTeamId;
+	}
+	public String getMatchId() {
+		return matchId;
+	}
+	public void setMatchId(String matchId) {
+		this.matchId = matchId;
+	}
+	public String getHomeTeamLongName() {
+		return homeTeamLongName;
+	}
+	public void setHomeTeamLongName(String homeTeamLongName) {
+		this.homeTeamLongName = homeTeamLongName;
+	}
+	public String getAwayTeamLongName() {
+		return awayTeamLongName;
+	}
+	public void setAwayTeamLongName(String awayTeamLongName) {
+		this.awayTeamLongName = awayTeamLongName;
+	}
+	public String getHomeTeamShortName() {
+		return homeTeamShortName;
+	}
+	public void setHomeTeamShortName(String homeTeamShortName) {
+		this.homeTeamShortName = homeTeamShortName;
+	}
+	public String getAwayTeamShortName() {
+		return awayTeamShortName;
+	}
+	public void setAwayTeamShortName(String awayTeamShortName) {
+		this.awayTeamShortName = awayTeamShortName;
+	}
+
+
+
 }
--- PMPI/src/com/project/ai/db/DatabaseHelper.java
+++ PMPI/src/com/project/ai/db/DatabaseHelper.java
@@ -6,7 +6,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -25,26 +28,86 @@ import com.project.ai.dataclasses.TeamInfo;
 import com.project.ai.interfaces.IBasicTeamsInfo;
 
 public class DatabaseHelper extends DBConnectionManager implements IBasicTeamsInfo {
-	
-	public static String EPL_COUNTRY_ID = "1729";
+
+	public static String EPL_LEAGUE_ID = "1729";
 
 	@Override
 	public ArrayList<TeamInfo> getAllTeams(String year) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
+	//Date should be in "yyyy-MM-dd hh:mm:ss" format. Use SimpleDateFormat and pass the string
 	@Override
 	public ArrayList<MatchInfo> getAllMatches(String date) {
-		return null;
+		Connection connection = getConnection();
+		ArrayList<MatchInfo> allMatches = new ArrayList<>();
+
+		String getAllMatchesOnThisDayQuery = "Select season, date, match_api_id, home_team_api_id, away_team_api_id, league_id "
+				+ "From Match Where date = ? And league_id = ?;";
+		PreparedStatement preparedStatement;
+		try {
+			preparedStatement = connection.prepareStatement(getAllMatchesOnThisDayQuery);
+			preparedStatement.setString(1, date);
+			preparedStatement.setString(2, EPL_LEAGUE_ID);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while(resultSet.next()) {
+				String season = resultSet.getString("season");
+				String dateResult = resultSet.getString("date");
+				String match_api_id = resultSet.getString("match_api_id");
+				String home_team_api_id = resultSet.getString("home_team_api_id");
+				String away_team_api_id = resultSet.getString("away_team_api_id");
+				String league_id = resultSet.getString("league_id");
+
+				String[] homeTeamNames = getTeamLongAndShortNames(home_team_api_id);
+				String[] awayTeamNames = getTeamLongAndShortNames(away_team_api_id);
+
+				MatchInfo matchInfo = new MatchInfo();
+				matchInfo.setHomeTeamId(home_team_api_id);
+				matchInfo.setHomeTeamLongName(homeTeamNames[0]);
+				matchInfo.setHomeTeamShortName(homeTeamNames[1]);
+
+				matchInfo.setAwayTeamId(away_team_api_id);
+				matchInfo.setAwayTeamLongName(homeTeamNames[0]);
+				matchInfo.setAwayTeamShortName(homeTeamNames[1]);
+
+				matchInfo.setMatchId(match_api_id);
+
+				allMatches.add(matchInfo);
+			}
+			preparedStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return allMatches;
 	}
 
 	@Override
 	public ArrayList<PlayerInfo> getAllPlayers(long teamId, String matchId) {
-		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
+	private String[] getTeamLongAndShortNames(String teamApiId) {
+		String[] teamNames = new String[2];
+		Connection connection = getConnection();
+		String getTeamNameQuery = "Select team_long_name, team_short_name From Team Where team_api_id = ?;";
+		PreparedStatement preparedStatement;
+		try {
+			preparedStatement = connection.prepareStatement(getTeamNameQuery);
+			preparedStatement.setString(1, teamApiId);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while(resultSet.next()) {
+				String team_long_name = resultSet.getString("team_long_name");
+				String team_short_name = resultSet.getString("team_short_name");
+				teamNames[0] = team_long_name;
+				teamNames[1] = team_short_name;
+			}
+			preparedStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return teamNames;
+	}
+
 	//Just for reference purpose, the following 2 methods will be removed
 	public void temporaryDatabaseFunction() {
 		Connection connection = getConnection();
@@ -60,12 +123,25 @@ public class DatabaseHelper extends DBConnectionManager implements IBasicTeamsIn
 			while(resultSet.next()) {
 				teamApiId = resultSet.getString("team_api_id");
 			}
+
+			//			String query = "SELECT sql FROM sqlite_master WHERE tbl_name = 'Match' AND type = 'table'";
+			//			PreparedStatement st = connection.prepareStatement(query);
+			//			ResultSet rs = st.executeQuery();
+			//			while(rs.next()) {
+			//				teamApiId = rs.getString("sql");
+			//			}
+
+			System.out.println("id: " + teamApiId);
+			findMatchResult(teamApiId);
+
+			resultSet.close();  
+			statement.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void findMatchResult(String teamApiId) {
 		int thisTeamGoalCount = 0;
 		int oppositionGoalCount = 0;
@@ -81,6 +157,8 @@ public class DatabaseHelper extends DBConnectionManager implements IBasicTeamsIn
 				String goals = resultSet.getString("goal");
 				String season = resultSet.getString("season");
 				String date = resultSet.getString("date");
+
+				System.out.println("date: " + date);
 
 				DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 				DocumentBuilder dBuilder;
