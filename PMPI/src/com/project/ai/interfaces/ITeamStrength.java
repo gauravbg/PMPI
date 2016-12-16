dcba7159fb868f76dd69ea12c08fbf1ccf7c78e5
Nipun Bayas <nipunbayas@yahoo.co.in>
2016-12-15 22:53:22 -0500

    Changed method name to getStandingsOfSeason

--- PMPI/src/com/project/ai/interfaces/ITeamStrength.java
+++ PMPI/src/com/project/ai/interfaces/ITeamStrength.java
@@ -3,23 +3,13 @@ package com.project.ai.interfaces;
 import java.util.HashMap;
 
 public interface ITeamStrength {
-
-	//Number of Wins
-	int getNofWinsHistory(int rangeMin, int rangeMax, String currentSeason, int howManyPrevSeasons);
-	
-	//Number of Draws
-	int getNofDrawsHistory(int rangeMin, int rangeMax, String currentSeason, int howManyPrevSeasons);
-		
-	//Number of Losses
-	int getNofLossesHistory(int rangeMin, int rangeMax, String currentSeason, int howManyPrevSeasons);
-	
 	
 	// residual matches difficulty
 	HashMap<String, int[]> getPreviousStandingsAllOpponents(String teamId, String matchId, int howManyPrevSeasons);
 
 	// Get the <standings, points, wins, draws, losses, GF, GA, GD> for a team for a
 	// particular season
-	HashMap<String, int[]> getStandingsOfPreviousSeason(String season);
+	HashMap<String, int[]> getStandingsOfSeason(String season);
 
 	// Returns final total points in previous seasons by that team
 	int getTotalPointsHistory(String teamId, String whichPrevSeason);
